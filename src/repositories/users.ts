5c300af375640cf9fdbac3373e4a5a07d00991f6
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-05-17 20:51:44 +0530

    Simplify the code

--- src/repositories/users.ts
+++ src/repositories/users.ts
@@ -17,10 +17,10 @@ export const findUserById = async (id) => {
 export const createUserData = async (id, username, email, picture) => {
   return prisma.user.create({
     data: {
-      id: id,
-      username: username,
-      email: email,
-      picture: picture,
+      id,
+      username,
+      email,
+      picture,
     },
   })
 }
@@ -28,12 +28,12 @@ export const createUserData = async (id, username, email, picture) => {
 export const updateUserData = async (id, username, email, picture) => {
   return prisma.user.update({
     where: {
-      id: id,
+      id,
     },
     data: {
-      username: username,
-      email: email,
-      picture: picture,
+      username,
+      email,
+      picture,
     },
   })
 }
@@ -49,7 +49,7 @@ export const deleteUserById = async (id) => {
 export const searchUserById = async (id) => {
   return prisma.user.findUnique({
     where: {
-      id: id,
+      id,
     },
   })
 }
